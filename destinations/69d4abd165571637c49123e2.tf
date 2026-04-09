28268dab5587c34cfdaf5a39e046d36b55dc84bf
Segment Git Sync <friends@segment.com>
2026-04-09 01:31:11 +0000

    Destination '69d4abd165571637c49123e2' modified by sthorat@segment.com

--- destinations/69d4abd165571637c49123e2.tf
+++ destinations/69d4abd165571637c49123e2.tf
@@ -17,7 +17,7 @@ resource "segment_destination" "id-69d4abd165571637c49123e2" {
   }
   name = "Iterable"
   settings = jsonencode({
-    apiKey                       = "••••••••••203a"
+    apiKey                       = "••••••••••40dd"
     mapAllPagesToSingleEventName = true
     trackAllPages                = true
     trackCategorizedPages        = true
